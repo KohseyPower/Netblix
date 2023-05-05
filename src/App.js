e9eccd7509e2fd86c76d1214bb29b30b54d9655a
Kohsey <kohsey.dufour@gmail.com>
2023-05-05 19:22:01 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import netblixLogo from "./img/netflix-logo.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <nav>
+      <div class="right-nav">
+        <div class="netblix-logo-container">
+          <img class="netblix-logo" src={netblixLogo} alt="react logo" />
+        </div>
+        <ul>
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+          <li>Browse by Languages</li>
+        </ul>
+      </div>
+      <div class="left-nav">
+      </div>
+    </nav>
+  )
 }
 
 export default App;
