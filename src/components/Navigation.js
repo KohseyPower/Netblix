1a11f329a419cb9518cf501c748e1774c1efa862
Kohsey Dufour <kohsey.dufour@gmail.com>
2023-08-16 18:33:01 +0200

    link in nav fixed

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,6 @@
 import './Navigation.css';
 import netblixLogo from "../img/netflix-logo.png";
+import { Link } from "react-router-dom";
 
 export default function Navigation() {
     return (
@@ -9,9 +10,9 @@ export default function Navigation() {
                     <img class="netblix-logo" src={netblixLogo} alt="react logo" />
                 </div>
                 <ul>
-                    <li className="section-li-balise"><a className="section-a-balise" href="#">Home</a></li>
-                    <li className="section-li-balise"><a className="section-a-balise" href="#">TV Shows</a></li>
-                    <li className="section-li-balise"><a className="section-a-balise" href="#">Movies</a></li>
+                    <li className="section-li-balise"><Link to="/">Home</Link></li>
+                    <li className="section-li-balise"><Link to="/tvshows">TV Shows</Link></li>
+                    <li className="section-li-balise"><Link to="/movies">Movies</Link></li>
                     <li className="section-li-balise"><a className="section-a-balise" href="#">New & Popular</a></li>
                     <li className="section-li-balise"><a className="section-a-balise" href="#">My List</a></li>
                     <li className="section-li-balise"><a className="section-a-balise" href="#">Browse by Languages</a></li>
