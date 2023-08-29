8e0df37569c7d63c5796b356385f2389c283f101
Kohsey <kohsey.dufour@gmail.com>
2023-08-29 19:17:46 +0200

    first movie advise + desc

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Navigation from './components/Navigation';
+import Home from "./pages/Home";
+import TVShows from "./pages/TVShows";
+import Movies from "./pages/Movies";
+import './App.css';
+
+function App() {
+    return (<>
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Lexend+Deca&display=swap');
+        </style>
+        <Navigation />
+        <Routes>
+            <Route path="/" element={< Home />} />
+            <Route path="/tvshows" element={< TVShows />} />
+            <Route path="/movies" element={< Movies />} />
+        </Routes> </>
+    )
+}
+
+export default App;     
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import React from "react";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+root.render( <
+    React.StrictMode >
+    <
+    BrowserRouter >
+    <
+    body >
+    <
+    App / >
+    <
+    /body> <
+    /BrowserRouter> <
+    /React.StrictMode>
 );
