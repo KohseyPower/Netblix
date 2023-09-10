c4c42fccc962e282b05f06f23889b65ac3616b8e
Kohsey <kohsey.dufour@gmail.com>
2023-09-10 22:19:52 +0200

    resize carousel begining

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -15,18 +15,57 @@ import carouselImg13 from '../img/new_releases_carousel/new_releases_carousel-im
 import carouselImg14 from '../img/new_releases_carousel/new_releases_carousel-img14.jpg';
 import carouselImg15 from '../img/new_releases_carousel/new_releases_carousel-img15.jpg';
 import "./Carousel.css";
+import { useState, useEffect, useRef } from 'react';
 
 
 
 export default function Carousel(props) {
+
+    // const [dimensions, setDimensions] = React.useState({
+    //     width: document.querySelector(".item").innerWidth,
+    //     height: document.querySelector(".item").innerHeight,
+    // });
+    // console.log(dimensions);
+    // const handleResize = () => {
+    //     setDimensions({
+    //         width: document.querySelector(".item").innerWidth,
+    //         height: document.querySelector(".item").innerHeight,
+    //     });
+    // }
+    // React.useEffect(() => {
+    //     window.addEventListener("resize", handleResize, false);
+    // }, []);
+
+
+
+    const itemRef = useRef(null);
+    const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+
+    const handleResize = () => {
+        if (itemRef.current) {
+            const { offsetWidth, offsetHeight } = itemRef.current;
+            setDimensions({ width: offsetWidth, height: offsetHeight });
+        }
+    };
+
+    useEffect(() => {
+        // Ajoutez l'écouteur de redimensionnement au chargement du composant
+        handleResize();
+        window.addEventListener('resize', handleResize);
+        return () => {
+            // Supprimez l'écouteur de redimensionnement lors du démontage du composant
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
         <>
             <h2>{props.title}</h2>
             <div className="wrapper">
                 <section id='section1' className='row-content'>
-                    <a href='#section2' className='arrow__btn scroll-btn'>‹</a>
+                    <a href='#section2' className='arrow__btn scroll-btn' >‹</a>
                     <div className="item">
-                        <img src={carouselImg1} alt="Describe Image"></img>
+                        <img src={carouselImg1} alt="Describe Image" ref={itemRef}></img>
                     </div>
                     <div className="item">
                         <img src={carouselImg2} alt="Describe Image"></img>
