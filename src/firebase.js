7c005f669d0ecebe87bc765dbcb9df5943fdba6b
Kohsey <kohsey.dufour@gmail.com>
2023-09-19 09:38:43 +0200

    setup firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCNDBNTGK7DTgT6EXZx7Xoh3RZWYAK8SZQ",
+    authDomain: "netblix-5ba89.firebaseapp.com",
+    projectId: "netblix-5ba89",
+    storageBucket: "netblix-5ba89.appspot.com",
+    messagingSenderId: "92318158976",
+    appId: "1:92318158976:web:e02370cc12d9255b9c5665",
+    measurementId: "G-NQYDHFLSN3"
+};
