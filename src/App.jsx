f90f4a3751ce70dce265b0c4dbd96518dfacaf2f
Kohsey Dufour <kohsey.dufour@gmail.com>
2023-09-19 12:28:19 +0200

    begin login screen

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,35 @@ import Home from "./pages/Home";
 import TVShows from "./pages/TVShows";
 import Movies from "./pages/Movies";
 import './App.css';
+import LoginScreen from "./pages/LoginScreen";
+
 
 function App() {
-    return (<>
-        <style>
+    const auth = null; // Replace with your authentication logic
+
+    return (
+        <div>
+            <style>
+                {`
             @import url('https://fonts.googleapis.com/css2?family=Lexend+Deca&display=swap');
-        </style>
-        <Navigation />
-        <Routes>
-            <Route path="/Netblix" element={< Home />} />
-            <Route path="/tvshows" element={< TVShows />} />
-            <Route path="/movies" element={< Movies />} />
-        </Routes> </>
-        
-    )
+          `}
+            </style>
+            {auth ? (
+                <>
+                    <Navigation />
+                    <Routes>
+                        <Route path="/Netblix" element={<Home />} />
+                        <Route path="/tvshows" element={<TVShows />} />
+                        <Route path="/movies" element={<Movies />} />
+                    </Routes>
+                </>
+            ) : (
+                <Routes>
+                    <Route path="/Netblix" element={<LoginScreen />} />
+                </Routes>
+            )}
+        </div>
+    );
 }
 
-export default App;     
+export default App;
---
+++ src/pages/LoginScreen.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './LoginScreen.css';
+
+export default function LoginScreen() {
+  return (
+    <div class="login-screen">
+      Login Screen
+    </div>
+  )
+}
