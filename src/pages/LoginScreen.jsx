578a851f3a4d856cc0256e91f64868b0f9c31091
Kohsey <kohsey.dufour@gmail.com>
2023-09-21 10:00:24 +0200

    sin in design

--- src/pages/LoginScreen.jsx
+++ src/pages/LoginScreen.jsx
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react';
 import './LoginScreen.css';
 import netflixLogo from '../img/netflix-logo.png';
-import { auth} from '../firebase';
+import { auth } from '../firebase';
 
 export default function LoginScreen() {
   const emailRef = useRef(null);
@@ -27,13 +27,22 @@ export default function LoginScreen() {
         <main className="login-main">
           <div className="signup-form-container">
             <form>
-              <h1>Sign In</h1>
-              <input ref={emailRef} placeholder="Email" type="email" />
-              <input ref={passwordRef} placeholder="Password" type="password" />
-              <button type="button" onClick={signIn}>
+              <h1 class="sign-in-h1">Sign In</h1>
+              <input className="sign-in-input" ref={emailRef} placeholder="Email" type="email" />
+              <input className="sign-in-input" ref={passwordRef} placeholder="Password" type="password" />
+              <div className="blank-between-inputs-and-btn"></div>
+              <button className="sign-in-btn" type="button" onClick={signIn}>
                 Sign In
               </button>
             </form>
+            <div className="signup-text">
+              <p>
+                New to Netflix? <span className="signup-link">Sign up now.</span>
+              </p>
+              <p>
+                This page is protected by Google reCAPTCHA to ensure you're not a bot. <span className="signup-link">Learn more.</span>
+              </p>
+            </div>
           </div>
         </main>
       </div>
