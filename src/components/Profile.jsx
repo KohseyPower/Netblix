7e077ba6fa890c40d2c8f5c2a06e13ec5402214d
Kohsey Dufour <kohsey.dufour@gmail.com>
2023-09-27 09:59:30 +0200

    profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,13 +3,15 @@ import "./Profile.css";
 
 function Profile() {
     return (
-        <div className="profile-options">
-            <div>
-                <h2>Account</h2>
-                <h2>Help Center</h2>
-            </div>
-            <div>
-                <h2>Sign out of Netblix</h2>
+        <div className="profile-container">
+            <div className="profile-options">
+                <div className="upper-profile">
+                    <h2>Account</h2>
+                    <h2>Help Center</h2>
+                </div>
+                <div className="lower-profile">
+                    <h2>Sign out of Netblix</h2>
+                </div>
             </div>
         </div>
     )
