f7347b1fbf3945acd073e02b29da48d33fd6bd32
Kohsey <kohsey.dufour@gmail.com>
2023-10-01 21:40:11 +0200

    fjdksl

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import "./Profile.css";
+import { useSelector } from 'react-redux';
+import { selectUser } from '../features/userSlice';
+import { auth } from '../firebase';
 
 function Profile() {
+    const user = useSelector(selectUser);
     return (
         <div className="profile-container">
             <div className="profile-options">
                 <div className="upper-profile">
-                    <h2>Account</h2>
-                    <h2>Help Center</h2>
+                    <h2 class="option">Account</h2>
+                    <h2 class="option">Help Center</h2>
                 </div>
                 <div className="lower-profile">
-                    <h2>Sign out of Netblix</h2>
-                </div>
+                    <h2 class="option"><a href={null} onClick={() => auth.signOut()}>Sign out of Netblix</a></h2>
+                </div> 
             </div>
         </div>
     )
